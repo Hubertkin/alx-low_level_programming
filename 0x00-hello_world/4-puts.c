7ae89dead6242276a12ba407cc445aac0bcde15b
Hubertkin <kingsleyocran@gmail.com>
2022-12-08 16:45:47 -0800

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *main - entry point of code and returns int 0
+ *Func displays a string to std output
+ *Return: 0 if executed successfully
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
