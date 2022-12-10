4438fa19f3232c3ebd0cff44f15bdfc23f06c91e
Hubertkin <kingsleyocran@gmail.com>
2022-12-09 22:10:48 -0800

    Hot fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,18 +14,17 @@ int main(void)
 		for (j = i + 1; j < 9; j++)
 		{
 			for (k = j + 1; k < 10; k++)
-			{	    
+			{
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-				if (i +j + k == 24)
-					break;
-				else
+				if (i + j + k < 24)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-
+				else
+					break;
 			}
 		}
 	}
