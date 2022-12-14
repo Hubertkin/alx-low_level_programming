0c1b9209b0da8395452a4201addcb3c5c3cd73b8
Hubertkin <kingsleyocran@gmail.com>
2022-12-14 04:14:22 -0800

    Hot fix  + one more

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - computes time for 24 hrs range
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int k;
+	int j;
+	int p;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (k = 0; k < 4; k++)
+		{
+			for (j = 0; j < 6; j++)
+			{
+				for (p = 0; p < 10; p++)
+				{
+					_putchar('i');
+					_putchar('k');
+					_putchar(':');
+					_putchar('j');
+					_putchar('p');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	return ;
+}
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * times_table - computes time for 24 hrs range
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i;
+	int k;
+	int j;
+	int p;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		p = 0;
+		for (k = 0; k < 10; k++)
+		{
+			p += i
+				if (p > 10)
+				{
+					j = p / 10;
+					_putchar(j + '0');
+					j = p % 10;
+					_putchar(j + '0');
+				} else
+					_putchar(p + '0');
+			if (k != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if ((p + i) < 10)
+					_putchar(' ');
+				else
+					break;
+			} else
+				break;
+		}
+	}
+}
