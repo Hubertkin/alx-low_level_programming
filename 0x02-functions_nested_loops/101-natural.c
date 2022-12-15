3115978ec8da0d3cc73adbea74e1dd75f8ad1ddb
Hubertkin <kingsleyocran@gmail.com>
2022-12-15 01:07:31 -0800

    adding more

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * addsum - computes the sum of func add3 + add5
+ * Return: res(sum of add3 and add5)
+ */
+
+void addsum(void)
+{
+	long int res;
+
+	res = add3 + add5;
+	printf("%d", res);
+}
+
+/**
+ * add5 - computes sum of multiples of 5 less than 1024
+ * Return: total sum (sum)
+ */
+
+int add5(void)
+{
+	int i;
+	int d5;
+	int sum;
+
+	d5 = 0;
+	sum = 0;
+	for (i = 0; i < 204; i++)
+	{
+		d5 += 5;
+		sum += d5;
+	}
+	return (sum);
+}
+
+/**
+ * add3 - computes sum of multipls of 3 less than 1024
+ * Return: total sum (sum)
+ */
+
+int add3(void)
+{
+	int i;
+	int d3;
+	int sum;
+
+	d3 = 0;
+	sum = 0;
+	for (i = 0; i < 341; i++)
+	{
+		d3 += 3;
+		sum += d3;
+	}
+	return (sum);
+}
