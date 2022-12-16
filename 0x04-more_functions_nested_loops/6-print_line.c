ebb68f13f709eec8868bac2e64811b1de5f03458
Hubertkin <kingsleyocran@gmail.com>
2022-12-16 06:10:52 -0800

    Hot fix + add

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints a straight line per input number
+ * @n: input of func
+ * Return: void
+ */
+void print_line(int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
