20e7aa3fbedb2ca4f82307ebb4b9eca6bd3c0ca5
Hubertkin <kingsleyocran@gmail.com>
2022-12-16 06:47:49 -0800

    adding more

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square per input number
+ * @size: input of func
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int k;
+
+	for (i = 0; i < n; i++)
+	{
+		for (k = 0; k < n; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (n == 0 || n < 0)
+		_putchar('\n');
+}
