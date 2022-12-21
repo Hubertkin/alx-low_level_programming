2f4f3362468a79697e5e2ae6156254c638f2382e
Hubertkin <kingsleyocran@gmail.com>
2022-12-20 20:17:43 -0800

    function file that swaps value of address

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps value of addres a and b
+ * @a: takes address of first input 
+ * @b: takes address of second input
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *d;
+
+	*d = *b;
+	*b = *a;
+	*a = *d;
+}
