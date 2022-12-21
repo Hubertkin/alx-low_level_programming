a552d021fd6530693ee04d3c02f617c8ad5dfa15
Hubertkin <kingsleyocran@gmail.com>
2022-12-20 19:05:34 -0800

    function file resets input to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the value of the input
+ * address to 98
+ * @n: takes address of variable as input
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
