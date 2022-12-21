3f8dd99fd3bb953e65ee5127c2a864f8e880f7d9
Hubertkin <kingsleyocran@gmail.com>
2022-12-20 21:52:53 -0800

    Adding more

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - computes length of string
+ * @s: takes address of input
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int d;
+
+	d = strlen(*s);
+	return (d);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts - computes length of string
+ * @str: takes address of input
+ * Return: length of string
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
